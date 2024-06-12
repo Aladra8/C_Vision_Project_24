9665cb8e8f3b9da8db21c5418b05f7b6444ea363
Luca Lavezzi <ll@MacBook-Air-di-Luca.local>
2024-06-12 16:52:33 +0200

    Removed ImageProcessor.h and ImageProcessor.cpp, updated CMakeLists.txt

--- src/ImageProcessor.cpp
+++
@@ -1,9 +0,0 @@
-#include "ImageProcessor.h"
-
-void ImageProcessor::processFrame(cv::Mat &frame) {
-    cv::Mat gray, blurred, edges;
-    cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
-    cv::GaussianBlur(gray, blurred, cv::Size(5, 5), 0);
-    cv::Canny(blurred, edges, 50, 150);
-    cv::imshow("Edges", edges);
-}
