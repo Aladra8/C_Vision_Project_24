c564aa6e5d30acdb00ab3dbd6ea7904f8931bef4
Luca Lavezzi <ll@MacBook-Air-di-Luca.local>
2024-07-18 23:42:14 +0200

    fixed a warning

--- include/VideoProcessor.h
+++ include/VideoProcessor.h
@@ -4,7 +4,7 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
-#include "Balltracking.h"
+#include "BallTracking.h"
 #include <vector>
 #include <iostream>
 
